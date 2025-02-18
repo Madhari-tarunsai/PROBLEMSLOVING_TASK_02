b3948e539329706398b081a3dc1643a706b031ac
Tarunsai Madhari <saitarunsai04062002@gmail.com>
2025-02-18 23:09:49 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,32 @@
+# list=[2,3,56,78,90]
+# sum_list=0
+# for i in list:
+#     sum_list+=i
+# print(list)
+# print("sum of the list = ",sum_list)   =>  [2, 3, 56, 78, 90]
+#                                                                 sum of the list =  229
+# #Finding the k Element which is present in a List.
+# list=[1,2,7,89,12,0,6]
+# k=1
+# for i in list:
+#     if(i==k):
+#         print("k value ",i,"is found")
+#         break
+#     else:
+#         print(i,"not found")   => k value 1 is found
+# # Wap to print duplicates and unique numbers in an array/List.
+# list=[10,3,56,10,45,3,7,56]
+# duplicate=[]
+# unique=[]
+# for i in list:
+#     if(i in unique ):
+#         duplicate.append(i)
+#     elif(i not in duplicate):
+#         unique.append(i)
+# print(list)
+# print(duplicate)
+# print(unique)
+# [10, 3, 56, 10, 45, 3, 7, 56]
+# [10, 3, 56]
+# [10, 3, 56, 45, 7]
+#
